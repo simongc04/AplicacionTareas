88b5d258e3da51e942016f6d4aff113045df6db0
simon <simonescuela9@gmail.com>
2024-11-21 09:38:04 +0000

    TiposTarea.kt movido a la carpeta de datos

--- app/src/main/java/com/simon/aplicaciontareas/datos/TiposTarea.kt
+++ app/src/main/java/com/simon/aplicaciontareas/datos/TiposTarea.kt
@@ -1,13 +1,10 @@
-package com.simon.aplicaciontareas
+package com.simon.aplicaciontareas.datos
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "tipos_tareas")
-data class TiposTareas(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
+data class TipoTarea(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0, 
     val titulo: String
 )
-
-
