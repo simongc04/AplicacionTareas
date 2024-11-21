9976790a709cee577917fbc2a659d183d8af3825
simon <simonescuela9@gmail.com>
2024-11-21 09:21:55 +0000

    implementacion de codigo

--- app/src/main/java/com/simon/aplicaciontareas/TiposTarea.kt
+++ app/src/main/java/com/simon/aplicaciontareas/TiposTarea.kt
@@ -1,2 +1,13 @@
 package com.simon.aplicaciontareas
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tipos_tareas")
+data class TiposTareas(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val titulo: String
+)
+
+
