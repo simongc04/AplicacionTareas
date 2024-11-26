7f7a5b82043b7a7850ca07309ab4744003b4ed87
simon <simonescuela9@gmail.com>
2024-11-26 09:26:44 +0000

    Añadi descripcion

--- app/src/main/java/com/simon/aplicaciontareas/datos/Task.kt
+++ app/src/main/java/com/simon/aplicaciontareas/datos/Task.kt
@@ -1,13 +1,16 @@
 package com.simon.aplicaciontareas.datos
 
 
+
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+
 
 @Entity(tableName = "tasks")
 data class Task(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
+    val description: String = "",
     val isCompleted: Boolean = false
 )
