55a135d75db77e443bae803a0b774a50e2d96605
simongc04 <simonescuela9@gmail.com>
2024-12-12 21:09:37 +0000

    añadido y realizado

--- app/src/main/java/com/simon/aplicaciontareas/datos/Task.kt
+++ app/src/main/java/com/simon/aplicaciontareas/datos/Task.kt
@@ -1,16 +1,24 @@
 package com.simon.aplicaciontareas.datos
 
-
-
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-
-@Entity(tableName = "tasks")
+@Entity(
+    tableName = "tasks",
+    foreignKeys = [
+        ForeignKey(
+            entity = TiposTareas::class,
+            parentColumns = ["id"],
+            childColumns = ["id_tipostareas"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Task(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    val name: String,
-    val description: String = "",
-    val isCompleted: Boolean = false
+    var descripcion: String,
+    var titulo: String,
+    var id_tipostareas: Int
 )
